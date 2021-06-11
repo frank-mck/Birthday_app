dc98238d8b0cb3836ec2ed85b05957d730b9c065
Frank Mckenna <fmckenna56330@gmail.com>
2021-06-11 11:24:28 +0100

    created post method, and is_it_my_birthday method

--- app.rb
+++ app.rb
@@ -1,10 +1,27 @@
 require 'sinatra'
-require "sinatra/reloader" if development?
+require "sinatra/reloader"
+require './lib/birthday'
+
+
+class Bday < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  enable :sessions
 
 get '/' do
   erb :index
 end
 
-get '/birthday' do
-  
-end 
+post '/birthday' do
+  @name = params[:name]
+  @day = params[:day] + params[:birthday_month]
+  @birthday = Birthday.new(@name, @day)
+  erb :birthday
+end
+
+
+ # start the server if ruby file executed directly
+ run! if app_file == $0
+end
---
+++ lib/birthday.rb
@@ -0,0 +1,49 @@
+ require 'date'
+
+ class Birthday 
+   attr_reader :name, :day, :date
+
+   def initialize(name, day)
+    @name = name
+    @day = day
+    @date = Date.parse(@day)
+   end
+
+   def is_it_my_birthday
+     if max == min
+      birthday_message
+     else
+      days_til_birthday
+     end
+   end
+
+   private
+
+   def birthday_message
+    "Happy Birthday #{@name}"
+   end
+
+   def days_til_birthday
+    "Your Birthday will be in #{days_left} day#{days_left > 1 ? 's' : ''}, #{@name}"
+   end
+
+   def days_left
+    max.to_i - min.to_i
+   end
+ 
+   def my_birthday_day
+    @date.strftime "%j"
+   end
+
+   def min
+    [my_birthday_day, current_day_of_year].min
+   end
+
+   def max 
+    [my_birthday_day, current_day_of_year].max
+   end
+
+   def current_day_of_year
+    DateTime.now.strftime "%j"
+   end
+  end
